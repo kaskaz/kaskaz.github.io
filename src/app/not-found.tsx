1a7de0a6913b44db284e9645f4ce037dfbcc91a0
Nuno Cascalho <ncascalho@gmail.com>
2023-08-16 20:02:32 +0100

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col justify-center h-full w-full">
+      <p className="text-center text-5xl lg:text-6xl font-semibold text-red-400">
+        &#60;404NotFound &#47;&#62;
+      </p>
+    </div>
+  );
+}
