fcba13cf2937584292b0ce0daaa1bb9cd150dbfb
Nuno Cascalho <ncascalho@gmail.com>
2025-12-04 17:19:13 +0000

    refactor aboutme page

--- src/app/(content)/aboutme/page.tsx
+++ src/app/(content)/aboutme/page.tsx
@@ -1,64 +1,15 @@
-import React from "react";
-
-type About = {
-	section: string
-	description: (string | React.ReactNode)[]
-}
-
-const CONTENT: Array<About> = [
-	{
-		section: "Short bio",
-		description: [
-			"From Seixal (Lisbon's south side), Portugal",
-			"Born in the 90's",
-			<>
-				Hobbyist trombone player (
-					<a
-						className="underline"
-						href="https://www.instagram.com/sunglasses.dixieband"
-						target="_blank"
-					>
-						check out my band!
-					</a>
-				)
-			</>
-		]
-	},
-	{
-		section: "Skills",
-		description: [
-			"Programming: Java/Kotlin, Javascript/Typescript, HTML, CSS, AngularJs, React, Python, C, HDL",
-			"Frameworks/Libraries: Spring, Hibernate, Mockito, JUnit, Docker, NextJs",
-			"Message queueing: Kafka, NATS/JetStream",
-			"Databases: MongoDB, Postgres",
-			"Project management: JIRA, Confluence, Stash",
-			"Tools/Infrastructure: ELK, Grafana, Datadog, SonarQube, Netlify, CircleCI, AWS (basic), Heroku"
-		]
-	},
-	{
-		section: "Interests",
-		description: [
-			"Keep working with Kotlin, React, CI/CD, Microservices, DDD, Event Sourcing, CQRS & BPMN",
-			"Work on new trends like AI or Web3",
-			"Learn new tech like Golang, Cloud services (AWS, GCP, etc), GraphQL, Kubernetes"
-		]
-	}
-];
+import ContentImage from "@/app/components/ContentImage";
+import { CONTENT } from "@/pages/abouts";
+import { MDXContent } from "@content-collections/mdx/react";
+import { allPages } from "content-collections";
+import { notFound } from "next/navigation";
 
 export default function AboutMe() {
-  return (
-		<>
-			<h1 className="text-2xl font-bold mb-5">About me</h1>
-			<ul className="text-lg">
-				{CONTENT.map((entry, i) => {
-					return (
-						<li key={i} className="mb-3">
-							<h2 className="text-xl font-semibold">{entry.section}</h2>
-							{entry.description.map((text, k) => <p key={k} className="ml-5">{text}</p>)}
-						</li>
-					);
-				})}
-			</ul>
-		</>
-  );
+const page = allPages.findLast(p => p.id == 'aboutme');
+
+  if (!page) {
+    notFound();
+  }
+
+  return <MDXContent code={page.mdx} components={{ ContentImage: ContentImage }} abouts={CONTENT} />;
 };
